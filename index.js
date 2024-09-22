3b51c76a4735cb08f889eecb7162c4e2e756790d
Ajay Deekonda <133995637+Ajaydeekonda@users.noreply.github.com>
2024-09-22 18:40:38 +0530

    added product.controller.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://haris2iftikhar:GClTzr15XhkjvN6k@backenddb.nrurtot.mongodb.net/Node-API?retryWrites=true&w=majority"
+    "mongodb+srv://ajaydeekonda:dikonda9@backenddb.mw8elgw.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB"
   )
   .then(() => {
     console.log("Connected to database!");
@@ -33,3 +33,5 @@ mongoose
   .catch(() => {
     console.log("Connection failed!");
   });
+
+
