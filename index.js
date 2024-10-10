876c7e70a3c0fecaec27a11c36d23608295c3e48
Ajay Deekonda <133995637+Ajaydeekonda@users.noreply.github.com>
2024-10-10 23:17:07 +0530

    added env file

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const productRoute = require("./routes/product.route.js");
+const dotenv = require("dotenv"); // Import dotenv
 const app = express();
+
+// Load environment variables from .env file
+dotenv.config();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use("/api/products", productRoute);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
+// Define the port, defaulting to 5000 if PORT is not set
+const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB using the URI from the environment variable
 mongoose
-  .connect(
-    "mongodb url paste here"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch(() => {
     console.log("Connection failed!");
-});
-
-
+  });
